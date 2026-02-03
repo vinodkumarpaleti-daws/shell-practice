a6b19d1b7afe0f8d15b33092747be59cdd327238
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 18:47:54 +0530

    New file added

---
+++ 09-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PRODUCTS=("Iphone", "Lenovo", "Sony")
+echo "Products are ${PRODUCTS[@]}"
+echo "Mobile phone is : ${PRODUCTS[0]}"
+echo "Laptop is : ${PRODUCTS[1]}"
+echo "TV is : ${PRODUCTS[2]}"
