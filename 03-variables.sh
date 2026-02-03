ed07ff9bee7298dc43ee0fe0e8c900f8bf5ea17a
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 17:01:00 +0530

    03-variables.sh file added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-PERSON1=VINOD # No space required in between Value and assignment operartor(=)
-PERSON2=KUMAR
+PERSON1=Ramesh # No space required in between Value and assignment operartor(=)
+PERSON2=Suresh
 
 echo "$PERSON1: Hello...$PERSON2, how are you doing?"
 echo "$PERSON2: Hi..$PERSON1, I'm good..how about you?"
+echo "$PERSON1: Which place are you living $PERSON2?"
+echo "$PERSON2: I'm living in Hyderabad $PERSON1."
