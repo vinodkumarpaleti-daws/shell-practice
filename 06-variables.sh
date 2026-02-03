43b793e6ca2182bb18a80a91bd64433c50d3f8db
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 17:51:50 +0530

    06-variables.sh file added

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,7 +3,8 @@
 START_TIME=$(date +%s)
 
 echo "script started executed at: $START_TIME"
-sleep 10
+sleep 13
 END_TIME=$(date +%s)
+echo "script execution ended at: $END_TIME"
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script executed in $TOTAL_TIME..sec"
