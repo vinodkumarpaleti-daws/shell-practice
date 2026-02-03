8bfe4f77a35cf5fe4d738f195da873539d9c85cb
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 17:22:43 +0530

    04-variables.sh file updated

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-PERSON1=$1 # No space required in between Value and assignment operartor(=)
-PERSON2=$2
+
+# No space required in between Value and assignment operartor(=)
+PERSON1=$1  # Here $1, $2 and $3 will accept the arguments on the CLI while executing the script.
+PERSON2=$2  # How to execute: sh 04-variables.sh
+PERSON3=$3
 
 echo "$PERSON1: Hello...$PERSON2, how are you doing?"
 echo "$PERSON2: Hi..$PERSON1, I'm good..how about you?"
 echo "$PERSON1: Which place are you living $PERSON2?"
 echo "$PERSON2: I'm living in Hyderabad $PERSON1."
+echo "$PERSON3: Come, please take your seat $PERSON1, $PERSON2"
