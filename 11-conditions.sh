c31a1f05bafe32f23c4d91983f56b7843851e47c
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 19:06:18 +0530

    New file added

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -7,6 +7,6 @@ if [ $Number -gt 10 ]; then
     echo "Entered Number $Number greater than 10"
 elif [ $Number -eq 10 ]; then
     echo "Entered Number $Number is equal 10"
-elif
+else
     echo "Entered Number $Number less than 10"
 fi
