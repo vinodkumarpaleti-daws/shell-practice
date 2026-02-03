c8af756476a0a4d099611b7fb9b5e71caa28a1ef
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 17:40:26 +0530

    05-variables.sh file added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,6 +5,6 @@
 echo "Hi, Please enter your username"
 read username
 echo "Please enter your password"
-read -s password
+read -s -p password
 
 echo "Welcome, $username..your password is $password" 
