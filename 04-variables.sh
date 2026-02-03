07463ea94d321206602960a6e42a5fda742ebecc
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 17:14:30 +0530

    04-variables.sh file added

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1 # No space required in between Value and assignment operartor(=)
+PERSON2=$2
+
+echo "$PERSON1: Hello...$PERSON2, how are you doing?"
+echo "$PERSON2: Hi..$PERSON1, I'm good..how about you?"
+echo "$PERSON1: Which place are you living $PERSON2?"
+echo "$PERSON2: I'm living in Hyderabad $PERSON1."
