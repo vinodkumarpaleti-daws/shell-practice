bc7d066d8159894cbc64a7545b4b5d366b936309
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-03 19:03:09 +0530

    New file added

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -5,8 +5,8 @@ read Number
 
 if [$Number -gt 10]; then
     echo "Entered Number $Number greater than 10"
-    elif [$Number -eq 10]; then
+elif [$Number -eq 10]; then
     echo "Entered Number $Number is equal 10"
-    elif [];
+elif [];
     echo "Entered Number $Number less than 10"
 fi
