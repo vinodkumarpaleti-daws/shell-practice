ca6d8cddd698bef81e8ecff429852b63f4b82f44
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-09 11:51:57 +0530

    14-logs.sh file added

--- 14-logs.sh
+++ 14-logs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/logs/shell-script/"
-LOG_FILE="$LOGS_FOLDER/$0.log"
+LOGS_FOLDER="/var/logs/shell-script/" # It will create the Logs folder in that directory.
+LOG_FILE="$LOGS_FOLDER/$0.log"  # It will create the Logs file in that directory.
 
 if [ $USERID -ne 0 ]; then
     echo "Run this script with root user access"
     exit 1
 fi
-mkdir -p $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER   # Creating the Logs Folder
 VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo "$2....FAILURE"
@@ -16,11 +16,11 @@ VALIDATE(){
         echo "$2....SUCCESS"
     fi
 }
-dnf install nginx -y &>> $LOG_FILE
+dnf install nginx -y &>> $LOG_FILE    # &>> $LOGS_FILE -- redirects the logs in the logs file.
 VALIDATE $? "Installing Nginx"
 
 dnf install mysql -y &>> $LOG_FILE
 VALIDATE $? "Installing Mysql"
 
-dnf install nodejs -y &>> $LOG_FILE
+dnf installl nodejs -y &>> $LOG_FILE
 VALIDATE $? "Installing nodejs"
