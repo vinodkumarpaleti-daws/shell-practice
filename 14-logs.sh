2b90505bd6ab028e1f5bfc0f3b95fcda7f5f0f2f
Vinod Kumar <kumarvinod.paleti@gmail.com>
2026-02-09 11:39:41 +0530

    14-logs.sh file added

---
+++ 14-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script/"
+LOG_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Run this script with root user access"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2....FAILURE"
+    else
+        echo "$2....SUCCESS"
+    fi
+}
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
